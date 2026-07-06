baa29c6b6874d6d89469883c1973b28f8279c97e
Marhs2 <wjdehddfl12@gmail.com>
2026-07-06 14:48:35 +0900

    Create user-activities.vue

---
+++ frontend/src/components/user-activities.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { onMounted } from "vue";
+import { GetUserActivities } from "../composables/useApi";
+
+const userActivities = ref([]);
+
+async function fetchUserActivities() {
+    const activities = await GetUserActivities();
+    userActivities.value = activities;
+}
+
+onMounted(() => {
+    fetchUserActivities();
+});
+</script>
+
+<template>
+    <div>
+        <ul>
+            <li v-for="activity in userActivities" :key="activity.id">
+                {{ activity.name }}
+            </li>
+        </ul>
+    </div>
+</template>
